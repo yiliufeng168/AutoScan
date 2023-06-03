bdf2970b3b06f523696236936c55dcd2ddb4faa0
yiliufeng <ylf@yiliufeng.net>
2023-06-03 21:52:48 +0800

    MOD: 优化执行逻辑

--- AutoScan.py
+++ AutoScan.py
@@ -1,14 +1,23 @@
+import argparse
 import nmap
 from config import RECEIVERS
 from util import send_mail
 
-nm = nmap.PortScanner()
-nm.scan(hosts='192.168.0.1/24', arguments='-sS -T4')
 
-while nm.still_scanning():
-    progress = nm.progress
-    print("Scan is", progress, "percent complete.")
+def progress_callback(_, progress):
+    print(f"Progress: {progress}%")
 
 
-for receiver in RECEIVERS:
-    send_mail(receiver, '主机扫描结果', nm.csv())
+def main(host):
+    nm = nmap.PortScanner()
+    nm.scan(hosts=host, arguments='-p- -sS -T4', callback=progress_callback)
+
+    for receiver in RECEIVERS:
+        send_mail(receiver, '主机扫描结果', nm.csv())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Scan hosts in LAN')
+    parser.add_argument('--host', type=str, default='', help='Host to scan')
+    args = parser.parse_args()
+    main(args.host)
