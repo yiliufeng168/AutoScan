2850319ffe2aa4e9126a15e2876777a4da142959
yiliufeng <ylf@yiliufeng.net>
2023-06-04 10:40:39 +0800

    MOD: change2

--- AutoScan.py
+++ AutoScan.py
@@ -1,18 +1,13 @@
-import argparse
 import os
+import sys
 
 from config import RECEIVERS
 from util import send_mail
 
-
-def main(host):
-    os.system('nmap -sP %s' % host)
+if __name__ == '__main__':
+    status = os.system("nmap " + " ".join(sys.argv[1:]))
+    if status != 0:
+        print("命令执行失败")
+        sys.exit(1)
     for receiver in RECEIVERS:
         send_mail(receiver, '主机扫描结果', "扫描完成")
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Scan hosts in LAN')
-    parser.add_argument('--host', type=str, default='', help='Host to scan')
-    args = parser.parse_args()
-    main(args.host)
