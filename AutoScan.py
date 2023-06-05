4b6581047813eb67e7647e2f08132baf1dc7e81b
yiliufeng <ylf@yiliufeng.net>
2023-06-05 11:16:17 +0800

    MOD: 修改邮件提示信息

--- AutoScan.py
+++ AutoScan.py
@@ -5,9 +5,10 @@ from config import RECEIVERS
 from util import send_mail
 
 if __name__ == '__main__':
-    status = os.system("nmap " + " ".join(sys.argv[1:]))
+    cmd = "nmap " + " ".join(sys.argv[1:])
+    status = os.system(cmd)
     if status != 0:
         print("命令执行失败")
         sys.exit(1)
     for receiver in RECEIVERS:
-        send_mail(receiver, '主机扫描结果', "扫描完成")
+        send_mail(receiver, '主机扫描结果', cmd + "扫描完成")
